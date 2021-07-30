ba5face8b079fc8f29f82ab419b44d72a37da6ea
Graham Barry <grahambarry@live.ie>
2021-07-30 03:08:28 +0100

    Added mailer vars and email and subject to mailer

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -2,10 +2,7 @@ class MessageMailer < ApplicationMailer
   default from: 'developer@grahambarryux.com'
 
   def contact_email
-
     @message = params[:args]
-    puts "#{@message} <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<MESSAGE MESSAGE MASSAGE"
-    # @url  = 'http://example.com/login'
-    mail(to: @message[:email], subject: 'Test Email')
+    mail(to: 'grahambarry@live.ie', subject: 'Contact Form GBUX')
   end
 end
